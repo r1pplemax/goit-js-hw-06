93405455e4e6a8f5fce805c84a51aacd086bd897
Ihor Riabko <igorriabko2@gmail.com>
2022-07-10 23:07:54 +0300

    Update task#1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const numberAllCategories = document.querySelector('ul').children.length;
-console.log(numberAllCategories);
+const categoryElement = document.querySelectorAll('.item')
+
+console.log(`Number of categories:${categoryElement.length}`);
+
+[...categoryElement].forEach(function (el, index) {
+    const nameOfCategory = el.querySelector('h2');
+    const numberOfCategory = el.querySelectorAll('li');
+    console.log(`Category: ${nameOfCategory.textContent}`);
+    console.log(`Elements: ${numberOfCategory.length}`);
+})
