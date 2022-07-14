9eaf9b2e52b60d28b43c7ba5a3061309026219c3
Ihor Riabko <igorriabko2@gmail.com>
2022-07-14 16:06:58 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const mainList = document.querySelector('ul');
+
+const listIngredients = ingredients.map(ingridient => {
+  const newEl = document.createElement('li')
+        newEl.textContent = ingridient
+        newEl.classList.add('item')
+  
+  console.log(newEl);
+})
+  .join(" ")
+
+mainList.append('li');
+
