20dea581a58817256a2ccaf9978805e0f53ff3a6
Ihor Riabko <igorriabko2@gmail.com>
2022-07-24 19:23:45 +0300

    make all task's

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}
+
+const body = document.querySelector('body')
+
+const btn = document.querySelector('button')
+
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const newColor = getRandomHexColor()
+  body.style.backgroundColor = newColor;
+  color.textContent = `${newColor}`;
 }
